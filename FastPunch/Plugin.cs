756b6fc6f9ebadf43e45414b6646324bb390f2fd
Temperz87 <58375877+Temperz87@users.noreply.github.com>
2023-04-10 00:35:54 -0400

    fix indentation
    How did this even happen???

--- FastPunch/Plugin.cs
+++ FastPunch/Plugin.cs
@@ -47,48 +47,48 @@ public static class Ensure_ArmNoChangieFromRed
 {
     public static bool Prefix()
     {
-		if (FistControl.Instance.currentPunch?.type != FistType.Heavy)
-		{
-			FistControl.Instance.ArmChange(1);
-			FistControl.Instance.currentPunch.Invoke("Update", 0);
-		}
-		return false;
+        if (FistControl.Instance.currentPunch?.type != FistType.Heavy)
+        {
+            FistControl.Instance.ArmChange(1);
+            FistControl.Instance.currentPunch.Invoke("Update", 0);
+        }
+        return false;
     }
 }
 
 [HarmonyPatch(typeof(FistControl), "Update")]
 public static class Ensure_ArmChangieToBlue
 {
-	public static void Postfix()
-	{
-		if (MonoSingleton<InputManager>.Instance.InputSource.Punch.WasPerformedThisFrame && FistControl.Instance.currentPunch.type != FistType.Standard)
-		{
-			FistControl.Instance.ArmChange(0);
-			FistControl.Instance.currentPunch.Invoke("Update", 0);
-		}
-	}
+    public static void Postfix()
+    {
+        if (MonoSingleton<InputManager>.Instance.InputSource.Punch.WasPerformedThisFrame && FistControl.Instance.currentPunch.type != FistType.Standard)
+        {
+            FistControl.Instance.ArmChange(0);
+            FistControl.Instance.currentPunch.Invoke("Update", 0);
+        }
+    }
 }
 
 [HarmonyPatch(typeof(Punch), nameof(Punch.BlastCheck))]
 public static class Ensure_KBBlast
 {
-	public static bool Prefix(Punch __instance)
-	{
-		if (MonoSingleton<InputManager>.Instance.InputSource.ChangeFist.IsPressed) // Only line of code really changed
-		{
-			Traverse punchTraverse = Plugin.GetPunchTraverse(__instance);
-			punchTraverse.Field("holdingInput").SetValue(false);
-			punchTraverse.Field("anim").GetValue<Animator>().SetTrigger("PunchBlast");
-			Vector3 position = MonoSingleton<CameraController>.Instance.GetDefaultPos() + MonoSingleton<CameraController>.Instance.transform.forward * 2f;
-			RaycastHit raycastHit;
-			if (Physics.Raycast(MonoSingleton<CameraController>.Instance.GetDefaultPos(), MonoSingleton<CameraController>.Instance.transform.forward, out raycastHit, 2f, LayerMaskDefaults.Get(LMD.EnvironmentAndBigEnemies)))
-			{
-				position = raycastHit.point - CameraController.Instance.gameObject.transform.forward * 0.1f;
-			}
-			Object.Instantiate<GameObject>(__instance.blastWave, position, MonoSingleton<CameraController>.Instance.transform.rotation);
-		}
-		return false;
-	}
+    public static bool Prefix(Punch __instance)
+    {
+        if (MonoSingleton<InputManager>.Instance.InputSource.ChangeFist.IsPressed) // Only line of code really changed
+        {
+            Traverse punchTraverse = Plugin.GetPunchTraverse(__instance);
+            punchTraverse.Field("holdingInput").SetValue(false);
+            punchTraverse.Field("anim").GetValue<Animator>().SetTrigger("PunchBlast");
+            Vector3 position = MonoSingleton<CameraController>.Instance.GetDefaultPos() + MonoSingleton<CameraController>.Instance.transform.forward * 2f;
+            RaycastHit raycastHit;
+            if (Physics.Raycast(MonoSingleton<CameraController>.Instance.GetDefaultPos(), MonoSingleton<CameraController>.Instance.transform.forward, out raycastHit, 2f, LayerMaskDefaults.Get(LMD.EnvironmentAndBigEnemies)))
+            {
+                position = raycastHit.point - CameraController.Instance.gameObject.transform.forward * 0.1f;
+            }
+            Object.Instantiate<GameObject>(__instance.blastWave, position, MonoSingleton<CameraController>.Instance.transform.rotation);
+        }
+        return false;
+    }
 }
 
 [HarmonyPatch(typeof(Punch), "Update")]
@@ -96,67 +96,67 @@ public static class Inject_PunchBehavior
 {
     public static bool Prefix(Punch __instance) // Yummi code that I basically copy and pasted
     {
-		Traverse punchTraverse = Plugin.GetPunchTraverse(__instance);
+        Traverse punchTraverse = Plugin.GetPunchTraverse(__instance);
         bool shopping = punchTraverse.Field("shopping").GetValue<bool>();
         bool holdingInput = punchTraverse.Field("holdingInput").GetValue<bool>();
         if (MonoSingleton<OptionsManager>.Instance.paused)
-		{
-			return false;
-		}
-		
+        {
+            return false;
+        }
+
         // The only big change here is that OR for checking whether or not the fists should punch
-		if (((__instance.type == FistType.Standard && MonoSingleton<InputManager>.Instance.InputSource.Punch.WasPerformedThisFrame) || (__instance.type == FistType.Heavy && InputManager.Instance.InputSource.ChangeFist.WasPerformedThisFrame)) && __instance.ready && !shopping && FistControl.Instance.fistCooldown <= 0f && FistControl.Instance.activated && !GameStateManager.Instance.PlayerInputLocked)
-		{
+        if (((__instance.type == FistType.Standard && MonoSingleton<InputManager>.Instance.InputSource.Punch.WasPerformedThisFrame) || (__instance.type == FistType.Heavy && InputManager.Instance.InputSource.ChangeFist.WasPerformedThisFrame)) && __instance.ready && !shopping && FistControl.Instance.fistCooldown <= 0f && FistControl.Instance.activated && !GameStateManager.Instance.PlayerInputLocked)
+        {
             float cooldownCost = punchTraverse.Field("cooldownCost").GetValue<float>();
             FistControl.Instance.weightCooldown += cooldownCost * 0.25f + FistControl.Instance.weightCooldown * cooldownCost * 0.1f;
-			FistControl.Instance.fistCooldown += FistControl.Instance.weightCooldown;
-			__instance.Invoke("PunchStart", 0);
-			punchTraverse.Field("holdingInput").SetValue(true);
-		}
-		if (holdingInput && MonoSingleton<InputManager>.Instance.InputSource.Punch.WasCanceledThisFrame)
-		{
-			punchTraverse.Field("holdingInput").SetValue(false);
-		}
-		float layerWeight = __instance.anim.GetLayerWeight(1);
-		if (shopping && layerWeight < 1f)
-		{
-			__instance.anim.SetLayerWeight(1, Mathf.MoveTowards(layerWeight, 1f, Time.deltaTime / 10f + 5f * Time.deltaTime * (1f - layerWeight)));
-		}
-		else if (!shopping && layerWeight > 0f)
-		{
-			__instance.anim.SetLayerWeight(1, Mathf.MoveTowards(layerWeight, 0f, Time.deltaTime / 10f + 5f * Time.deltaTime * layerWeight));
-		}
-		if (!MonoSingleton<InputManager>.Instance.PerformingCheatMenuCombo() && MonoSingleton<InputManager>.Instance.InputSource.Fire1.WasPerformedThisFrame && shopping)
-		{
-			__instance.anim.SetTrigger("ShopTap");
-		}
-		if (punchTraverse.Field("returnToOrigRot").GetValue<bool>())
-		{
-			__instance.transform.parent.localRotation = Quaternion.RotateTowards(__instance.transform.parent.localRotation, Quaternion.identity, (Quaternion.Angle(__instance.transform.parent.localRotation, Quaternion.identity) * 5f + 5f) * Time.deltaTime * 5f);
-			if (__instance.transform.parent.localRotation == Quaternion.identity)
-			{
-				punchTraverse.Field("returnToOrigRot").SetValue(false);
-			}
-		}
-		if (FistControl.Instance.shopping && !shopping)
-		{
-			__instance.ShopMode();
-		}
-		else if (!FistControl.Instance.shopping && shopping)
-		{
-			__instance.StopShop();
-		}
-		if (__instance.holding && __instance.heldItem)
-		{
-			if (!__instance.heldItem.noHoldingAnimation && FistControl.Instance.forceNoHold <= 0)
-			{
-				__instance.anim.SetBool("SemiHolding", false);
-				__instance.anim.SetBool("Holding", true);
-				return false;
-			}
-			__instance.anim.SetBool("SemiHolding", true);
-		}
-        
-		return false;
-	}
+            FistControl.Instance.fistCooldown += FistControl.Instance.weightCooldown;
+            __instance.Invoke("PunchStart", 0);
+            punchTraverse.Field("holdingInput").SetValue(true);
+        }
+        if (holdingInput && MonoSingleton<InputManager>.Instance.InputSource.Punch.WasCanceledThisFrame)
+        {
+            punchTraverse.Field("holdingInput").SetValue(false);
+        }
+        float layerWeight = __instance.anim.GetLayerWeight(1);
+        if (shopping && layerWeight < 1f)
+        {
+            __instance.anim.SetLayerWeight(1, Mathf.MoveTowards(layerWeight, 1f, Time.deltaTime / 10f + 5f * Time.deltaTime * (1f - layerWeight)));
+        }
+        else if (!shopping && layerWeight > 0f)
+        {
+            __instance.anim.SetLayerWeight(1, Mathf.MoveTowards(layerWeight, 0f, Time.deltaTime / 10f + 5f * Time.deltaTime * layerWeight));
+        }
+        if (!MonoSingleton<InputManager>.Instance.PerformingCheatMenuCombo() && MonoSingleton<InputManager>.Instance.InputSource.Fire1.WasPerformedThisFrame && shopping)
+        {
+            __instance.anim.SetTrigger("ShopTap");
+        }
+        if (punchTraverse.Field("returnToOrigRot").GetValue<bool>())
+        {
+            __instance.transform.parent.localRotation = Quaternion.RotateTowards(__instance.transform.parent.localRotation, Quaternion.identity, (Quaternion.Angle(__instance.transform.parent.localRotation, Quaternion.identity) * 5f + 5f) * Time.deltaTime * 5f);
+            if (__instance.transform.parent.localRotation == Quaternion.identity)
+            {
+                punchTraverse.Field("returnToOrigRot").SetValue(false);
+            }
+        }
+        if (FistControl.Instance.shopping && !shopping)
+        {
+            __instance.ShopMode();
+        }
+        else if (!FistControl.Instance.shopping && shopping)
+        {
+            __instance.StopShop();
+        }
+        if (__instance.holding && __instance.heldItem)
+        {
+            if (!__instance.heldItem.noHoldingAnimation && FistControl.Instance.forceNoHold <= 0)
+            {
+                __instance.anim.SetBool("SemiHolding", false);
+                __instance.anim.SetBool("Holding", true);
+                return false;
+            }
+            __instance.anim.SetBool("SemiHolding", true);
+        }
+
+        return false;
+    }
 }
